c6b69d263aac4b337014c371789ff4b94e218ffe
Ravi kumar <raviluhaniwal318@gmail.com>
2024-10-08 16:20:11 +0530

    anil

--- Anil/Javascript/Operators.js
+++ Anil/Javascript/Operators.js
@@ -16,24 +16,24 @@
 
 // // modulus == remainder
 // let num3 = 3
-// console.log(num2 % num3 ) 
+// console.log(num2 % num3 )
 
-// // increment 
-// // a) prefix 
-    // let num4 = 10
-    // let inc = ++num4 
+// // increment
+// // a) prefix
+// let num4 = 10
+// let inc = ++num4
 
-    // console.log(num4)
-    // console.log(inc)
+// console.log(num4)
+// console.log(inc)
 
-    // // postfix 
-    // let num5 = 10
-    // let inc1 = num5++ 
-    // let inc2 = num5++ 
+// // postfix
+// let num5 = 10
+// let inc1 = num5++
+// let inc2 = num5++
 
-    // console.log(num5)
-    // console.log(inc1)
-    // console.log(inc2)
+// console.log(num5)
+// console.log(inc1)
+// console.log(inc2)
 
 // //  2. Assignmnt operators
 // // =
@@ -47,16 +47,16 @@
 // c = 70
 // console.log(c)
 
-// let x = 40 
+// let x = 40
 // let y = 53
 
-// let v = y + 10 
+// let v = y + 10
 // y %= 2
 // console.log(y)
 
 //  3. Comparision operators
-// == : check the value of our variables 
-// === : check value and the type  
+// == : check the value of our variables
+// === : check value and the type
 // !=  opposite of ==
 // !==  opposite ===
 // >
@@ -64,23 +64,46 @@
 // >=
 // <=
 
-let x = 10
-let y = 20
-let z = "10" 
-let a = 10
+// let x = 10;
+// let y = 20;
+// let z = "10";
+// let a = 10;
 
 // let c = x != z
 // let d = x !== z
 // // console.log(c)
 // console.log(d)
 
-
-let g = x > 86
-console.log(g)
-
-
+// let g = x > 86;
+// console.log(g);
 
 //  4. Logical operators
 // and &&
 // or ||
 // not !
+
+// AND op
+// true &&  true = true
+// true &&  false = false
+// false &&  true = false
+// false &&  false = false
+
+//  OR op
+// true &&  true = true
+// true &&  false = true
+// false &&  true = true
+// false &&  false = false
+
+let x = 10;
+let y = 20;
+let z = "10";
+let a = 10;
+
+// let cp = x == y && y == z;
+// console.log(cp)
+
+let cp = x == y || !(z == a);
+console.log(cp);
+
+
+
---
+++ Anil/Javascript/statements.js
@@ -0,0 +1,35 @@
+//  If else Statements : it is a statement which is run when our condition is true.
+// Multiple condtions : ww are used  else if
+
+//  Syntax
+// if (conditions){
+
+// }else{
+// default statement
+// }
+
+// let x = 12;
+// if (x > 10) {
+//   console.log("x is greater");
+// } else {
+//   console.log("x is smaller");
+// }
+
+let x = 10;
+if (x > 10) {
+  console.log("x is greater");
+} else if (x == 10) {
+  console.log(" x is equal to 10");
+} else {
+  console.log("x is smaller");
+}
+
+let name1 = "anil";
+
+if (name1 == "anil") {
+  console.log("Not equal to anil");
+} else if (name1 == "anil") {
+  console.log(" equakl tp anil");
+} else {
+  console.log("something wrong");
+}
