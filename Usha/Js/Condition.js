545ba8903e659fe2eb1a0fbee260ade0492d4fc3
Ravi kumar <raviluhaniwal318@gmail.com>
2024-10-09 14:37:26 +0530

    if alse

---
+++ Usha/Js/Condition.js
@@ -0,0 +1,32 @@
+// Statements : if alse
+//  Which is run when our condition is true .
+
+//  synatax
+// if (condiiton) {
+//     // code to be executed
+// } else {
+// }
+
+//  synatax
+// if (condiiton) {
+//     // code to be executed
+// } else if (condition){
+//  code
+// } else{
+//  default condition
+// }
+
+let x = 20;
+if (x > 20) {
+  console.log("x is greater than 20");
+} else if (x < 20) {
+  console.log("x is smaller than 20");
+} else if (x == 20) {
+  console.log("x is eqaul to 20");
+} else {
+  console.log("something eror");
+}
+
+// 1. Calculator
+// 2. Signal
+//  3 . Driving or not Driving
