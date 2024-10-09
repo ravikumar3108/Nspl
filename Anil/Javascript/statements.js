69196e677f0c1de1efef0f455c9bba5f43d6e0e3
Ravi kumar <raviluhaniwal318@gmail.com>
2024-10-09 16:37:46 +0530

    if else

--- Anil/Javascript/statements.js
+++ Anil/Javascript/statements.js
@@ -33,3 +33,37 @@ if (name1 == "anil") {
 } else {
   console.log("something wrong");
 }
+
+// User Input == written always a string.
+// Convert string into number we can use parseInt method
+
+// let userinp = prompt("Enter Your name")
+// console.log(userinp)
+
+// let cal1 = parseInt(prompt("Enter your First Number"));
+// let cal2 = parseInt(prompt("Enter your Second  Number"));
+// let op = prompt("Enter your Operator");
+
+// if (op == "+") {
+//   console.log(cal1 + cal2);
+// } else if (op == "-") {
+//   console.log(cal1 - cal2);
+// } else if (op == "*") {
+//   console.log(cal1 * cal2);
+// } else if (op == "/") {
+//   console.log(cal1 / cal2);
+// } else {
+//   console.log("operator is not matched");
+// }
+
+let dr = parseInt(prompt("Enter your age"));
+
+if (dr >= 18 && dr <= 65) {
+  console.log("You are eligible for driving");
+} else if (dr < 18) {
+  console.log("you are not eligible fo driving");
+} else if (dr > 65) {
+  console.log("you are not eligible fo driving");
+} else {
+  console.log("you are immortal");
+}
