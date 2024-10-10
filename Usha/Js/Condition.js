83a62265c3ebf34fa479bb30e6f446c19143027a
Ravi kumar <raviluhaniwal318@gmail.com>
2024-10-10 10:47:37 +0530

    condtions

--- Usha/Js/Condition.js
+++ Usha/Js/Condition.js
@@ -30,3 +30,24 @@ if (x > 20) {
 // 1. Calculator
 // 2. Signal
 //  3 . Driving or not Driving
+
+//  User input :- always return a string
+// prompt()
+//  parseInt() :- which is used to convert string into integer
+
+let num1 = prompt("Enter first number", "Enter number");
+let num = parseInt(num1);
+let num2 = parseInt(prompt("Enter Second number"));
+let op = prompt("Enter Operator");
+
+if (op == "+") {
+  console.log("Add is :", num + num2);
+} else if (op == "-") {
+  console.log(num - num2);
+} else if (op == "*") {
+  console.log(num * num2);
+} else if (op == "/") {
+  console.log(num / num2);
+} else {
+  console.log("operator not match");
+}
