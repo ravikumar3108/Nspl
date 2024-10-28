35a1f37dcd777ecf4fabdf0939a1da063cb4e251
Ravi kumar <raviluhaniwal318@gmail.com>
2024-10-28 16:24:07 +0530

    bg

---
+++ Reactjs/usha/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor=""></label>
+        <input type="text" name="" id="" />
+      </form>
+    </div>
+  )
+}
+
+export default Form
--- Reactjs/usha/src/components/Home.jsx
+++ Reactjs/usha/src/components/Home.jsx
@@ -1,12 +1,28 @@
 import React from "react";
 import Navbar from "./Navbar";
+import image from "./images/mountain-meditate-4.webp";
 
-function Home(){
-    return(
-        <>
-        <h1 className="para">Heloo</h1>
-        </>
-    )
+function Home() {
+  let name = "Usha";
+  let age = 20;
+
+  return (
+    <>
+      <h1 className="para">Heloo</h1>
+      {/*  Image  */}
+      <img src={image} alt="" />
+      {/*  inline csss */}
+      <h1
+        style={{
+          color: "green",
+          paddingLeft: "30px",
+        }}
+      >
+        {/* Apply js into the Html content  */}
+        Hi my name is {name}. my age is {age}{" "}
+      </h1>
+    </>
+  );
 }
 
 export default Home;
