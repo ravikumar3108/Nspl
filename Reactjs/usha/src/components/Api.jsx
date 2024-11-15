cfe2ee18cabb85d6756dd353d6c66a8bfe27fbfb
Ravi kumar <raviluhaniwal318@gmail.com>
2024-11-15 15:34:36 +0530

    api

--- Reactjs/usha/src/components/Api.jsx
+++ Reactjs/usha/src/components/Api.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { json } from "react-router-dom";
 
 function Api() {
@@ -9,15 +9,48 @@ function Api() {
   //  Asynochnous functions
   //    Promises
   async function handlegetData() {
-    const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await fetch("https://dummyjson.com/products");
     const jsondata = await data.json();
-    // console.log(jsondata);
-    setApiData(jsondata);
+    setApiData(jsondata.products);
+    console.log(jsondata.products);
   }
+
+  //  Arrow function
+  // const arrFun = ()=>{
+
+  // }
+
+  //   Useffect Hook :- it is the side effect of our components
+
+  useEffect(()=>{
+    handlegetData()
+  },[]);
 
   return (
     <>
-      <button onClick={handlegetData}>Api Data fetch</button>
+      <div
+        className="pr"
+        style={{ display: "flex", justifyContent: "space-between" }}
+      >
+        {ApiData.map((item) => {
+          return (
+            <>
+              <div
+                className="it"
+                style={{
+                  border: "1px solid black",
+                  width: "33%",
+                }}
+              >
+                <img src="" alt="" />
+                <h1>{item.title}</h1>
+                <p>{item.description}</p>
+                <button>Add to cart</button>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </>
   );
 }
